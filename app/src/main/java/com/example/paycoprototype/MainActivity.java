244259749b09550a74a8a498cbb3e9aafad0bbc0
emmanuelly15 <emmanuellynoma@gmail.com>
2022-05-05 21:11:48 +0200

    Application with bar and action

--- app/src/main/java/com/example/paycoprototype/MainActivity.java
+++ app/src/main/java/com/example/paycoprototype/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.paycoprototype;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bn; //way to the Navigation bar in activity2
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      //  setContentView(T.layout.activity2_main);
 
         //creating objects
         TextView username=(TextView) findViewById(R.id.username);
         TextView password=(TextView) findViewById(R.id.password);
         Button signinbtn=(Button)  findViewById(R.id.submitbtn);
         CheckBox staybnt= (CheckBox) findViewById(R.id.staybtn);
+        bn= findViewById(R.id.bottomnav);
+        bn.setOnNavigationItemSelectedListener(bottomnavMethod);
 
         //Now the action listeners
 
@@ -49,4 +61,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //method for the navigation listener
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomnavMethod= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment frangment1=null;//how to call new fragment
+
+           //switch to select the item selected here
+            switch(menuItem.getItemId())
+            {
+                case R.id.scan:
+                    frangment1= new Scanner();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,frangment1).commit();
+            return false;
+        }
+              };
 }
